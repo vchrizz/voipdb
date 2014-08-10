d33728e3482666765a4eba3715f4f3773c68c486
git@github.com <mail@chil.at>
2014-08-10 21:51:16 +0200

    display more information about sipusers

--- voip/views.py
+++ voip/views.py
@@ -13,6 +13,17 @@ import logging
 #logger = logging.getLogger(__name__)
 logger = logging.getLogger('django.request')
 
+if DEBUG:
+    import logging, logging.handlers
+    logfile = "/tmp/django-ldap-debug.log"
+    my_logger = logging.getLogger('django_auth_ldap')
+    my_logger.setLevel(logging.DEBUG)
+ 
+    handler = logging.handlers.RotatingFileHandler(
+       logfile, maxBytes=1024 * 500, backupCount=5)
+ 
+    my_logger.addHandler(handler)
+
 import random
 def pwgen(size=16):
     str = []
